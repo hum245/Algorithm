52d77a4f52186f8430af8cd72b1d4fee0198f098
hum245 <hum12393@gmail.com>
2022-01-18 23:36:57 +0900

    Dynamic Programming

--- 백준11054.py
+++ 백준11054.py
@@ -1,24 +1,30 @@
 n = int(input())
 array = list(map(int, input().split()))
 
-dp = [1]*n
+dp_increase = [1]*n
+dp_decrease = [1]*n
 
 for i in range(n):    
     for j in range(i): 
-        if array[j] < array[i] and dp[i] < dp[j]+1:
-            dp[i] = dp[j]+1
+        if array[j] < array[i] and dp_increase[i] < dp_increase[j]+1:
+            dp_increase[i] = dp_increase[j]+1
+
+# for i in range(n):
+#     for j in range(i):
+#         if array[i]<array[j] and dp_decrease[i]<=dp_decrease[j]:
+#             dp_decrease[i] += 1
+
+for i in range(n-1, -1, -1):    
+    for j in range(n-1, i, -1): 
+        if array[j] < array[i] and dp_decrease[i] < dp_decrease[j]+1:
+            dp_decrease[i] = dp_decrease[j]+1
 
 
-for k in range(dp.index(max(dp))+1, n):
-    dp[k] = 1
+result = [0]*n
+for i in range(n):
+    result[i] = dp_increase[i] + dp_decrease[i]
 
-
-for i in range(dp.index(max(dp)), n):
-    for j in range(i):
-        if array[i]<array[j] and dp[i]<=dp[j]:
-            dp[i] = dp[i-1]+1
-
-
-
-print(dp)
-print(max(dp))
+print(dp_increase)
+print(dp_decrease)
+print(result)
+print(max(result)-1)
