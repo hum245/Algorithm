c017bb9d9b89345f887d84cee9681550119e670c
siha0718 <hum12393@gmail.com>
2022-03-04 01:41:50 +0900

    해결완료

--- 완전탐색/백준1697.py
+++ 완전탐색/백준1697.py
@@ -1,7 +1,7 @@
 from collections import deque
 n, k = map(int, input().split())
 
-d = [0] * 1000001
+visit = [0] * 1000001
 
 def bfs(n):
     queue = deque()
@@ -9,11 +9,29 @@ def bfs(n):
     while queue:
         v = queue.popleft()
         if v==k:
-            print(d[v])
+            print(visit[v])
             return
         for next in (v-1,v+1,v*2):
-            if 0<=next<100001 and not d[next]:
-                d[next]=d[v]+1
+            if 0<=next<100001 and not visit[next]:
+                visit[next] = visit[v]+1
                 queue.append(next)
                 
 bfs(n)
+
+
+
+#  bfs 기본 코드 
+
+# def bfs(graph, start_node):
+#     visit = list()
+#     queue = list()
+    
+#     queue.append(start_node)
+    
+#     while queue:
+#         node = queue.pop(0)
+#         if node not in visit:
+#             visit.append(node)
+#             queue.extend(graph[node])
+            
+#     return visit
